e222f5c0e1a4c2d75e8cf081d5b267742931865f
George Clayton Bennett <george.bennett@memphistn.gov>
2026-08-28 05:20:05 -0500

    conweb messaging dialed with extra space at the top

--- src/maxson_gui_utils/external_web_launch.py
+++ src/maxson_gui_utils/external_web_launch.py
@@ -36,6 +36,7 @@ def launch_configured_website(path:Path|str|None=None,service:str|None=None,item
     # If the user left it blank, or it's purely whitespace, use the default path
     if not url or not str(url).strip():
         config_mngr.set(service=service,item=item,value="",overwrite=False) # allows retrieval of edited value
+        console_stderr.print("")
         console_stderr.print("Configured URL is None. Configuration file mutated, keys created in config file: {path}")
         console_stderr.print(f'config_mngr.set(service="{service}",item="{item}",value="",overwrite=False)')
         console_stderr.print("")
