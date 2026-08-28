c81eac805b6405cf750867e2a66aead783019365
George Clayton Bennett <george.bennett@memphistn.gov>
2026-08-28 05:04:08 -0500

    inject config opportunity

--- src/maxson_gui_utils/external_web_launch.py
+++ src/maxson_gui_utils/external_web_launch.py
@@ -30,12 +30,17 @@ def launch_configured_website(path:Path|str|None=None,service:str|None=None,item
         path = CONFIG_PATH
 
     config_mngr = DworshakConfig(path = path)
-    config_mngr.set(service=SERVICE,item=item,value="",overwrite=False) # allows retrieval of edited value
-    console_stderr.print(f'config_mngr.set(service="{SERVICE}",item="{item}",value="",overwrite=False)')
     url = config_mngr.get(service=SERVICE,item=item) # allows retrieval of edited value
 
     # If the user left it blank, or it's purely whitespace, use the default path
     if not url or not str(url).strip():
+        config_mngr.set(service=SERVICE,item=item,value="",overwrite=False) # allows retrieval of edited value
+        console_stderr.print("Configured URL is None. Configuration file mutated, keys of opportunity generated with:")
+        console_stderr.print(f'config_mngr.set(service="{SERVICE}",item="{item}",value="",overwrite=False)')
+        console_stderr.print("")
+        console_stderr.print("To set the value, run dworshak-config CLI like this.")
+        console_stderr.print(f'dworshak-config set --service "{SERVICE}" --item "{item}" --value <WEBSITE>  --path "{path}"')
+        console_stderr.print("")
         return None
 
     launch_web_url(url)
