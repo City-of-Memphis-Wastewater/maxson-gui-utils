dc36ca9daf457ae5d1016daf16dc211c67b87362
City of Memphis Wastewater <george.bennett@memphistn.gov>
2026-08-29 10:02:04 -0500

    add resource.ref for data for icons, woth traversible handling

--- src/maxson_gui_utils/resources.py
+++ src/maxson_gui_utils/resources.py
@@ -3,15 +3,27 @@ from __future__ import annotations
 
 from pathlib import Path
 from importlib.resources import files
+from importlib.abc import Traversable
+
+from contextlib import contextmanager
+from importlib.resources import as_file
+
 from .context import IMPORT_NAME
 
-def resource_path(*parts: str):
+
+def resource_path(*parts: str) -> Traversable:
     return files(IMPORT_NAME).joinpath("data", *parts)
 
 
-def read_resource(*parts: str) -> str:
-    return resource_path(*parts).read_text()
+def read_resource(*parts: str, encoding: str = "utf-8") -> str:
+    return resource_path(*parts).read_text(encoding=encoding)
 
 
 def read_resource_bytes(*parts: str) -> bytes:
     return resource_path(*parts).read_bytes()
+
+@contextmanager
+def resource_file(*parts: str)->Path:
+    resource = resource_path(*parts)
+    with as_file(resource) as path:
+        yield path
